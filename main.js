eded7d9df3608685a6bf6737dbdc88e899ed3c9f
Rakhim bc2025-2 <AbdulRakhim.Abdulkhanov@lnu.edu.ua>
2026-03-26 13:46:47 +0200

    asd

---
+++ main.js
@@ -0,0 +1,14 @@
+const currency = 'usd';
+const date_from = '20220707';
+const date_to = '20220719';
+ 
+const url = new URL('https://bank.gov.ua/');
+url.pathname = '/NBU_Exchange/exchange_site';
+url.searchParams.set('start', date_from);
+url.searchParams.set('end', date_to);
+url.searchParams.set('valcode', currency);
+url.searchParams.set('sort', 'exchangedate');
+url.searchParams.set('order', 'desc');
+url.searchParams.append('json', '');
+
+console.log(url.toString());
